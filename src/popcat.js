4fdaafca7a32ee34db87c694137951e72d050ef0
frost1118 <66143704+frost1118@users.noreply.github.com>
2021-01-25 13:00:43 +0900

    popcat-web1.1

--- src/popcat.js
+++ src/popcat.js
@@ -1,15 +1,29 @@
-const cat = document.getElementById("popcat");
-const audio = document.getElementById("popsound");
+const mcat = document.getElementById("mpopcat");
+const maudio = document.getElementById("mpopsound");
+const dcat = document.getElementById("dpopcat");
+const daudio = document.getElementById("dpopsound");
 
-function press(event) {
-    cat.src="src/img/popcat_click.png";
-    audio.src="src/audio/click.mp3";
-    audio.play();
+function mpress(event) {
+    mcat.src="src/img/popcat_click.png";
+    maudio.src="src/audio/click.mp3";
+    maudio.play();
 }
 
-function release(event) {
-    cat.src="src/img/popcat_default.png";
+function mrelease(event) {
+    mcat.src="src/img/popcat_default.png";
 }
 
-cat.addEventListener("mousedown", press);
-cat.addEventListener("mouseup", release);
+function dpress(event) {
+    dcat.src="src/img/popcat_click.png";
+    daudio.src="src/audio/click.mp3";
+    daudio.play();
+}
+
+function drelease(event) {
+    dcat.src="src/img/popcat_default.png";
+}
+
+mcat.addEventListener("touchstart", mpress);
+mcat.addEventListener("touchend", mrelease);
+dcat.addEventListener("mousedown", dpress);
+dcat.addEventListener("mouseup", drelease);
